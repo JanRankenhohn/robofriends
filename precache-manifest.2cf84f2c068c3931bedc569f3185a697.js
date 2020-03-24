7880d9298e9996df08187929a29df96a7e2cf2e3
johannaverena <hanni.hanna@web.de>
2020-03-24 18:12:40 +0100

    Updates

---
+++ precache-manifest.2cf84f2c068c3931bedc569f3185a697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0bf94830e4ce128cc0acb56f1156cd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c25e63cbb4e2bfdf01c2",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6434a77ce601bea3eaea",
+    "url": "/robofriends/static/js/2.25dc1f92.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.25dc1f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25e63cbb4e2bfdf01c2",
+    "url": "/robofriends/static/js/main.af44ae28.chunk.js"
+  },
+  {
+    "revision": "5677d8ff5cb4c373cfaa",
+    "url": "/robofriends/static/js/runtime-main.c712519d.js"
+  }
+]);
